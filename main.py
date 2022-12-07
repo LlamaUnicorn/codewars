5e882ef8f794afaba3fad577068caccf815856e7
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2022-12-07 14:58:20 +0000

    Installing pytest is hell

--- main.py
+++ main.py
@@ -1,17 +1,78 @@
-# Complete the solution so that it reverses the string passed into it.
+# install pytest
 
-# 'world'  =>  'dlrow'
-# 'word'   =>  'drow'
+class BankAccount:
+  def __init__(self, id):
+    self.id = id
+    self.balance = 0
 
-# https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+  def withdraw(self, amount):
+    if self.balance >= amount:
+      self.balance -= amount
+      return True
+    return False
 
-# My convoluted solution
-def solution(string):
-    plist = list(string)
-    reversed = plist[::-1]
-    return "".join(reversed)
+  def deposit(self, amount):
+    self.balance += amount
+    return True
 
+# from math import sqrt
 
-# Solution
-# def solution(str):
-#   return str[::-1]
+# def square_eq_solver(a, b, c):
+#    result = []
+#    discriminant = b * b - 4 * a * c
+
+#    if discriminant == 0:
+#        result.append(-b / (2 * a))
+#    elif discriminant > 0:  # <--- изменили условие, теперь
+#                            # при нулевом дискриминанте
+#                            # не будут вычисляться корни
+#        result.append((-b + sqrt(discriminant)) / (2 * a))
+#        result.append((-b - sqrt(discriminant)) / (2 * a))
+
+#    return result
+
+# def show_result(data):
+#    if len(data) > 0:
+#        for index, value in enumerate(data):
+#            print(f'Корень номер {index+1} равен {value:.02f}')
+#    else:
+#        print('Уравнение с заданными параметрами не имеет корней')
+
+# import unittest
+
+# class SquareEqSolverTestCase(unittest.TestCase):
+#    def test_no_root(self):
+#        res = square_eq_solver(10, 0, 2)
+#        self.assertEqual(len(res), 0)
+
+#    def test_single_root(self):
+#        res = square_eq_solver(10, 0, 0)
+#        self.assertEqual(len(res), 1)
+#        self.assertEqual(res, [0])
+
+#    def test_multiple_root(self):
+#        res = square_eq_solver(2, 5, -3)
+#        self.assertEqual(len(res), 2)
+#        self.assertEqual(res, [0.5, -3])
+
+# def test_no_root():
+#    res = square_eq_solver(10, 0, 2)
+#    assert len(res) == 0
+
+# def test_single_root():
+#    res = square_eq_solver(10, 0, 0)
+#    assert len(res) == 1
+#    assert res == [0]
+
+# def test_multiple_root():
+#    res = square_eq_solver(2, 5, -3)
+#    assert len(res) == 3
+#    assert res == [0.5, -3]
+    
+# def main():
+#    a, b, c = map(int, input('Пожалуйста, введите три числа через пробел: ').split())
+#    result = square_eq_solver(a, b, c)
+#    show_result(result)
+
+# if __name__ == '__main__':
+#    main()
