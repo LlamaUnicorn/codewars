9c04bf2922e0ea94826cc1a13b702b61e13124fb
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2023-01-16 05:42:42 +0000

    PhoneWords: still figuring how to split input

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-def translate():
-    pass
+# TODO: Get the letter from string
+# TODO: Get the longest letter from string
+# TODO: Transform the number to the letter with the maximum value
+#         "777777" -->  "sq"` and `"7717777" --> "qs"`
+# TODO: 
+# TODO: 
+# TODO: 
+
+
+def translate(str_):
+    # try-catch
+    print('str_:', str_)
+    if str_ in dict_abc:
+        return dict_abc[str_]
+
 
 def splitter_(inp):
-    to_str = str(inp)
+    return str(inp)
 
-dict_2 = {222: "c",
-          22: "b",
-          2: "a",
-          333: "f",
-          33: "e",
-          3: "d"
+dict_abc = {'222': "c",
+          '22': "b",
+          '2': "a",
+          '333': "f",
+          '33': "e",
+          '3': "d",
+          '7777': 's',
+          '777': 'r',
+          '77': 'q',
+          '7': 'p'
          }
 
-test = 223
+test = 7777
 
-if test in dict_2:
-    print(dict_2[test])
-    
+splitted = splitter_(test)
+print('splitted:', splitted)
+transformed_text = translate(splitted)
+print('transformed:', transformed_text)
