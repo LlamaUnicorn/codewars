c6b3e3b15a22a1020a737f03aa4a72c33efd43d0
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2023-01-17 06:41:48 +0000

    PhoneWords: Filled the buttons dict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # DONE: Get the letter from string
+# DONE: Fill the dict_abc with all buttons
 # TODO: Split the input into two letters
 # TODO: Get the longest letter from string
 # TODO: Transform the number to the letter with the maximum value
@@ -10,7 +11,7 @@
 
 def translate(str_):
     # try-catch
-    print('str_:', str_)
+    print("str_:", str_)
     if str_ in dict_abc:
         return dict_abc[str_]
 
@@ -18,21 +19,39 @@ def translate(str_):
 def splitter_(inp):
     return str(inp)
 
-dict_abc = {'222': "c",
-          '22': "b",
-          '2': "a",
-          '333': "f",
-          '33': "e",
-          '3': "d",
-          '7777': 's',
-          '777': 'r',
-          '77': 'q',
-          '7': 'p'
-         }
+dict_abc = {
+    "0": " ",
+    "222": "c",
+    "22": "b",
+    "2": "a",
+    "333": "f",
+    "33": "e",
+    "3": "d",
+    "444": "i",
+    "44": "h",
+    "4": "g",
+    "555": "l",
+    "55": "k",
+    "5": "j",
+    "666": "o",
+    "66": "n",
+    "6": "m",
+    "7777": "s",
+    "777": "r",
+    "77": "q",
+    "7": "p",
+    "888": "v",
+    "88": "u",
+    "8": "t",
+    "9999": "z",
+    "999": "y",
+    "99": "x",
+    "9": "w",
+    }
 
-test = 7777
+test = 888
 
 splitted = splitter_(test)
-print('splitted:', splitted)
+print("splitted:", splitted)
 transformed_text = translate(splitted)
-print('transformed:', transformed_text)
+print("transformed:", transformed_text)
