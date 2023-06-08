fbdb4af61dbb04cff4a7451e2f1517da345c2870
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2023-06-08 08:58:06 +0000

    7 - youre a square solved

--- 7kyu/You're a square.py
+++ 7kyu/You're a square.py
@@ -1,14 +1,14 @@
 import math
 
-
-number = int(input())
-def square(val):
-    return math.sqrt(number)
-try:
-    square(number)
-except ValueError:
-    print("It's negative number")
-
 def is_square(n):    
-    return False # fix me
+    if n < 0:
+        return False
+    sqrt_num = math.sqrt(n)
+    return sqrt_num.is_integer()
+
+# solutions
+
+# import math
+# def is_square(n):
+#     return n > -1 and math.sqrt(n) % 1 == 0;
 
