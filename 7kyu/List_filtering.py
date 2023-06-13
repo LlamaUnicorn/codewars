a0f2e3150a0bf7114877b2468fdd50c91f9e86de
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2023-06-13 04:01:13 +0000

    list_filtering not done

--- 7kyu/List_filtering.py
+++ 7kyu/List_filtering.py
@@ -1,2 +1,9 @@
 def filter_list(l):
     'return a new list with the strings filtered out'
+    for i in l:
+        if type(i) != int:
+            print(i)
+            l.remove["i"]
+
+l = [1, 2, 'a', 'b']), [1, 2]
+filter_list(l)
--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-def phone_words(string_of_nums):
-    keypad = {
-        '2': 'abc',
-        '3': 'def',
-        '4': 'ghi',
-        '5': 'jkl',
-        '6': 'mno',
-        '7': 'pqrs',
-        '8': 'tuv',
-        '9': 'wxyz'
-    }
-    nums = list(string_of_nums)
-    result = ""
-    current_letter = ""
-    count = 0
-    for i, num in enumerate(nums):
-        if num == "1":
-            result += current_letter
-            count = 0
-            current_letter = ""
-        elif num == "0":
-            result += current_letter + " "
-            current_letter = ""
-            count = 0
-        elif i > 0 and num == nums[i-1]:
-            count += 1
-            if count > len(keypad[num])-1:
-                count = 0
-                result += current_letter
-            if current_letter != "" and current_letter[-1] == "1":
-                current_letter = current_letter[:-1]
-            else:
-                current_letter = keypad[num][count]
-        else:
-            if current_letter != "":
-                result += current_letter[-1]
-            current_letter = keypad[num][0]
-            count = 0
-    result += current_letter
-    return result.lower()
-
-
-a = phone_words("443355555566604466690277733099966688")
-print(a)
+def filter_list(l):
+    print(f'{l=}')
+    'return a new list with the strings filtered out'
+    for i in l:
+        print(f'{i=}')
+        if type(i) != int:
+            print(i)
+            l.remove(i)
+    print(f'{l=}')
+    return l
