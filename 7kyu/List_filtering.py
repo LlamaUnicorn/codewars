107bea5a706f37bbee00d72eeafe43b1b21f9763
LlamaUnicorn <holidaynight@gmail.com>
2023-06-14 06:19:05 +0300

    7 - List_filtering done

--- 7kyu/List_filtering.py
+++ 7kyu/List_filtering.py
@@ -1,9 +1,10 @@
 def filter_list(l):
     'return a new list with the strings filtered out'
-    for i in l:
-        if type(i) != int:
-            print(i)
-            l.remove["i"]
+    return [i for i in l if not isinstance(i, str)]
+    # return [i for i in l if type(i) == int]
+    # return [i for i in l if isinstance(i, int)]
 
-l = [1, 2, 'a', 'b']), [1, 2]
+
+l = [1, 2, 'a', 'b']
 filter_list(l)
+print(filter_list(l))
