8d499a172c2a8c1f32d67a4544bec93a23c763be
LlamaUnicorn <holidaynight@gmail.com>
2023-07-01 10:25:13 +0300

    7 - max_diff_easy solved

--- 7kyu/max_diff_easy.py
+++ 7kyu/max_diff_easy.py
@@ -1,5 +1,9 @@
-# def max_diff(lst):
-    # if not lst or lst == 0:
-        # return 0
-def add_numbers(a, b):
-    return a + b
+def max_diff(lst):
+    if len(lst) <= 1:
+        return 0
+
+    max_value, min_value = max(lst), min(lst)
+    return max_value - min_value
+
+# def max_diff(list):
+#     return max(list) - min(list) if list else 0
--- 7kyu/tests/max_diff_easy_test.py
+++ 7kyu/tests/max_diff_easy_test.py
@@ -4,7 +4,16 @@ import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from max_diff_easy import add_numbers
+from max_diff_easy import max_diff
 
-def test_add_numbers():
-    assert add_numbers(2, 3) == 5
+
+@pytest.mark.parametrize("lst, expected", [
+    ([], 0),
+    ([5], 0),
+    ([1, 2, 3, 4], 3),
+    ([1, 2, 3, -4], 7),
+    ([0, 1, 2, 3, 4, 5, 6], 6),
+])
+
+def test_max_diff(lst, expected):
+    assert max_diff(lst) == expected
