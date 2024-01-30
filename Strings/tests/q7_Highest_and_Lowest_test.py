d645e227c024f7ed6f9ea91dbce69f14edb5db30
LlamaUnicorn <holidaynight@gmail.com>
2024-01-30 06:49:57 +0300

    try again

--- Strings/tests/q7_Highest_and_Lowest_test.py
+++ Strings/tests/q7_Highest_and_Lowest_test.py
@@ -1,7 +1,8 @@
 import os
 import sys
+from hypothesis.strategies import just, text, lists, integers
+
 from hypothesis import given, example, strategies as st
-from hypothesis.strategies import just, text
 
 from Strings.q7_Highest_and_Lowest import high_and_low
 
@@ -9,19 +10,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 
 @given(just('8 3 -5 42 -1 0 0 -9 4 7 4 -4'))
-def test_high_and_low(words):
-    result = high_and_low(words)
+def test_high_and_low_example(numbers):
+    result = high_and_low(numbers)
     assert result == '42 -9', f"Incorrect answer for \"8 3 -5 42 -1 0 0 -9 4 7 4 -4\""
 
 
-# @given(just('y'))
-# def test_no_vowels_get_count(words):
-#     result = high_and_low(words)
-#     assert result == 0, f"Incorrect answer for \"y\""
-#
-#
-# @given(text())
-# def test_generated_get_count(words):
-#     result = high_and_low(words)
-#     expected = sum(1 for char in words if char in 'aeiou')
-#     assert result == expected, f"Incorrect answer for \"generator\""
+@given(lists(integers(), min_size=1).map(lambda x: ' '.join(map(str, x))))
+def test_high_and_low(numbers):
+    result = high_and_low(numbers)
+    ints = list(map(int, numbers.split(' ')))
+    expected_result = f'{max(ints)} {min(ints)}'
+    assert result == expected_result, f'Expected "{expected_result}", but got "{result}"'
