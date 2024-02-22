1c9e8e6a1ba6c0b4778387920bbda0c1a5938b2f
LlamaUnicorn <holidaynight@gmail.com>
2024-02-22 06:50:17 +0300

    22.02.2024 q6 create phone number test is not working.

--- arrays/q7_Sum_of_odd_numbers.py
+++ arrays/q7_Sum_of_odd_numbers.py
@@ -10,11 +10,24 @@
 #
 # 1 -->  1
 # 2 --> 3 + 5 = 8
+# 27
+# 64
+
+# Pattern Recognition: The first step in problems like these is to look for a pattern.
+# You’ve already been given the sums of the first two rows. Try calculating the sums of the next few rows and see if you can spot a pattern.
+
+# Mathematical Representation: Once you’ve spotted a pattern, the next step is to represent it mathematically.
+# This often involves figuring out a formula that describes the pattern you’ve observed.
+
+# Proof: After you’ve come up with a mathematical representation of the pattern, you should try to prove it.
+# This step ensures that the pattern you’ve observed holds for all cases and is not just a coincidence.
+
+# Implementation: Finally, once you’ve proven your formula, you can implement it in code to calculate
+# the sum of the nth row.
 
 
 def row_sum_odd_numbers(n):
-    # for i in range(1, 30)
-    ...
+    return n ** 3
 
 
 print(row_sum_odd_numbers(2))
