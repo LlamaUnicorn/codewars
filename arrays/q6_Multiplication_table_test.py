84bdac077340ebbfa0c102071f87b01c65a353c5
LlamaUnicorn <holidaynight@gmail.com>
2024-03-16 18:39:32 +0300

    16.03.2024 q6 create phone number test is not working.

---
+++ arrays/q6_Multiplication_table_test.py
@@ -0,0 +1,16 @@
+from q6_Multiplication_table import multiplication_table
+
+import string
+from hypothesis.strategies import just, text, lists, integers
+
+from hypothesis import given, example, strategies as st
+
+test_cases = {
+    3: [[1, 2, 3], [2, 4, 6], [3, 6, 9]],
+}
+
+
+@given(st.sampled_from(list(test_cases.keys())))
+def test_multiplication_table_example(word):
+    result = multiplication_table(word)
+    assert result == test_cases[word], f"Incorrect answer for \"{word}\""
